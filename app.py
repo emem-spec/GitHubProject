d4ce2152f9ee8e66d439029a90af988a22613714
adnbsn <adrien.besson75@gmail.com>
2026-01-06 02:13:40 +0100

    Update English text in app.py

--- app.py
+++ app.py
@@ -4,23 +4,20 @@ import streamlit as st
 import quant_b_module
 import quant_a_module
 
-# Configuration Globale de la page (Doit être la première commande Streamlit)
-st.set_page_config(page_title="Projet Finance - Groupe 12", layout="wide")
+st.set_page_config(page_title="Project Finance - Group 12", layout="wide")
 
-# Sidebar de Navigation
+# Sidebar of Navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Aller vers", ["Accueil", "Module Quant A (Mono-Actif)", "Module Quant B (Portefeuille)"])
+page = st.sidebar.radio("Go to", ["Menu", "Module Quant A (Mono-Actif)", "Module Quant B (Portefeuille)"])
 
-if page == "Accueil":
-    st.title("Projet Python pour la Finance 📈")
-    st.write("Bienvenue sur notre plateforme d'analyse financière.")
-    st.write("Utilisez le menu à gauche pour naviguer entre les modules.")
-    st.info("👈 Sélectionnez un module dans la barre latérale.")
+if page == "Menu":
+    st.title("Project Python Git - Group 12")
+    st.write("Welcome on our web platform for financial analysis.")
+    st.write("Use the left menu to navigate")
+    st.info("👈 Select a module from the sidebar.")
 
 elif page == "Module Quant A (Mono-Actif)":
-    # On appelle la fonction du fichier quant_a_module.py
     quant_a_module.run_quant_a()
 
 elif page == "Module Quant B (Portefeuille)":
-    # On appelle la fonction du fichier quant_b_module.py
     quant_b_module.run_quant_b()
